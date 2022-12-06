748330b72797f24f99687ce98f6283472bebb230
MisterEJ <eitvis.stul@gmail.com>
2022-12-06 12:46:57 +0200

    Day6/cleaned

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,9 +1,6 @@
 module Day6() where
 
 import System.IO
-import Control.Monad (when)
-import Data.List.Split
-import Data.Monoid
 
 solve :: IO ()
 solve = do
@@ -15,13 +12,7 @@ solve' :: String -> Int
 solve' [] = 0
 solve' (x:xs) = if check (take 14 (x:xs)) then 0 else solve' xs + 1
 
-
 check :: String -> Bool
 check [] = True
 check [_] = True
 check (x:xs) = x `notElem` xs && check xs
-
-
-check' :: String -> Char -> Int
-check' [] _ = 0
-check' (x:xs) c = (if x == c then 1 else 0) + check' xs c
