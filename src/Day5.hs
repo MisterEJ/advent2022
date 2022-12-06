f897082e540aa482b661dffb0daab7494bcdba9a
MisterEJ <eitvis.stul@gmail.com>
2022-12-06 12:55:12 +0200

    Day5/cleaned

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,21 +1,16 @@
 module Day5() where
 
 import System.IO
-import Control.Monad (when)
 import Data.List.Split
-import Data.Monoid
 
 solve :: IO ()
 solve = do
     handle <- openFile "input5" ReadMode
     content <- hGetContents handle
     let parsed = parse content
-    print $ uncurry solve' parsed
+    print $ uncurry (foldl solve'') parsed
 
 data Instruction = Instruction Int Int Int deriving (Show)
-
-solve' :: [String] -> [Instruction]  -> [String]
-solve' l i = foldl solve'' l i
 
 solve'' :: [String] -> Instruction -> [String]
 solve'' l (Instruction c f t) = do
@@ -24,7 +19,7 @@ solve'' l (Instruction c f t) = do
     [if x == f then taken else if x == t then given else l !! x | x<-[0..8]]
 
 move :: String -> String -> Int -> String
-move s1 s2 i = (take i s1) ++ s2
+move s1 s2 i = take i s1 ++ s2
 
 parse:: String -> ([String], [Instruction])
 parse s = do
@@ -48,15 +43,6 @@ parse s = do
             let spl = splitOn " " x
             Instruction (read $ spl !! 1) (read (spl !! 3)-1) (read (spl !! 5)-1) : parseIns xs
 
---parseIns :: String -> [Instruction]
-
 clean :: String -> String
 clean [] = []
 clean (x:xs) = if x == ' ' then clean xs else x : clean xs
-
-push :: Char -> [Char] -> [Char]
-push x xs = x : xs
-
-pop :: [Char] -> (Char, [Char])
-pop [] = ('-', [])
-pop (x:xs) = (x, xs)
